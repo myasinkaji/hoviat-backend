9d46e591d1f9d8622c4c4beaf2045ecaa9a003fb
Mohammad Yasin <myasinkaji@gmail.com>
2020-12-22 02:52:45 +0330

    prviding backend's infrastructure with spring boot

---
+++ src/main/java/ir/markaz/hoviat/HoviatApplication.java
@@ -0,0 +1,13 @@
+package ir.markaz.hoviat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoviatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoviatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/markaz/hoviat/HoviatApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.markaz.hoviat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoviatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
